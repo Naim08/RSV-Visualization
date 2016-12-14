320f16cdfd3c6be5da8449f695c1892706518e16
naim08 <mmiah@fordham.edu>
2016-12-14 15:25:04 -0500

    updated json

--- rsvdemo/views.py
+++ rsvdemo/views.py
@@ -1,11 +1,36 @@
 from django.shortcuts import render
 import os
 import pandas as pd
+import json, csv
 # Create your views here.
 from django.http import HttpResponse
 
 
 def index(request):
     path = os.getcwd() + '/media/outputUPDATED.csv'
-    rsvfile = pd.read_csv(path)
-    return render(request, 'index.html', {'rsvfile': rsvfile})
+    f = open(path)
+    df = {
+        'Subject ID': [],
+        'Site ID': [],
+        'Platform': [],
+        'App Version': [],
+         'When': [],
+        'Question ID': [],
+        'Question Text': [],
+        'Response ID': [],
+        'Response Text': [],
+        'Therm': []
+        }
+    csv_f = csv.reader(f)
+    for row in csv_f:
+        df['Subject ID'].append(unicode(row[0], errors='replace'))
+        df['Site ID'].append(unicode(row[1], errors='replace'))
+        df['Platform'].append(unicode(row[2], errors='replace'))
+        df['App Version'].append(unicode(row[3], errors='replace'))
+        df['When'].append(unicode(row[4], errors='replace'))
+        df['Question ID'].append(unicode(row[5], errors='replace'))
+        df['Question Text'].append(unicode(row[6], errors='replace'))
+        df['Response ID'].append(unicode(row[7], errors='replace'))
+        df['Response Text'].append(unicode(row[8], errors='replace'))
+        df['Therm'].append(unicode(row[9], errors='replace'))
+    return render(request, 'index.html', {'rsvfile':json.dumps(df)})
