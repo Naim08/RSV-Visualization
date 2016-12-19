1415306800af3a1e6553bdc27124220af4ce2371
naim08 <mmiah@fordham.edu>
2016-12-19 18:47:56 -0500

    added login and filter

--- rsvdemo/views.py
+++ rsvdemo/views.py
@@ -4,8 +4,12 @@ import pandas as pd
 import json, csv
 # Create your views here.
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
-
+# Create your views here.
+# this login required decorator is to not allow to any  
+# view without authenticating
+@login_required(login_url="login")
 def index(request):
     path = os.getcwd() + '/media/outputUPDATED.csv'
     f = open(path)
