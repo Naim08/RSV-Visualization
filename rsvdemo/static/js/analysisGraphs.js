eaec3a3fbaa8a500f5c017b65099273ec59127cc
hashemselim <hashem.selim@gmail.com>
2016-12-20 17:29:21 -0500

    edit to login, and adding labels to graph data

--- rsvdemo/static/js/analysisGraphs.js
+++ rsvdemo/static/js/analysisGraphs.js
@@ -480,7 +480,8 @@ function aaf(useFilter) {
                 Actual: "#29AFDF",
                 Forecasted : "#ED2835"
             },
-            type: 'bar'
+            type: 'bar',
+            labels: true
         },
         subchart: {
             show: true
@@ -538,7 +539,8 @@ function sf(useFilter) {
                 Actual: "#29AFDF",
                 Forecasted : "#ED2835"
             },
-            type: 'spline'
+            type: 'spline',
+//            labels: true
         },
         subchart: {
             show: true
@@ -600,7 +602,8 @@ function af(useFilter) {
                 Actual: "#29AFDF",
                 Forecasted : "#ED2835"
             },
-            type: 'line'
+            type: 'line',
+            labels: true
         },
         subchart: {
             show: true
@@ -667,7 +670,8 @@ function srf(useFilter) {
                 Actual: "#29AFDF",
                 Forecasted : "#ED2835"
             },
-            type: 'bar'
+            type: 'bar',
+            labels: true
         },
         subchart: {
             show: false,
