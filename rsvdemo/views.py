f8696d68cdc66889bdf1f52b6261fee859328e7b
naim08 <mmiah@fordham.edu>
2016-12-21 14:48:46 -0500

    DONEDONE

--- rsvdemo/views.py
+++ rsvdemo/views.py
@@ -84,29 +84,35 @@ def prepareData(request):
             driver.get('http://localhost:8000/rsvdemo/generateGraph/')
             driver.save_screenshot(os.getcwd() + '/media/rsvfiles/website.png')
             driver.quit()
+            
             mediapath = os.getcwd() + '/media/rsvfiles/filtersheet.xlsx'
             workbook = xlsxwriter.Workbook(mediapath)
             aaf = 'Application Access Frequency'
             sf = 'Symptom Frequency'
             srf = 'Response Frequency'
-            aqf = 'Adhoc Query Frequency'
+            aqf = 'Adhoc Query Frequency' 
             worksheet = workbook.add_worksheet(aaf)
             worksheet2 = workbook.add_worksheet(sf)
             worksheet3 = workbook.add_worksheet(aqf)
             worksheet4 = workbook.add_worksheet(srf)
             bold = workbook.add_format({'bold': 1})
             sfHeaders = []
+            adHeaders = []
             sfHeaders.append('Dates')
+            adHeaders.append('Dates')
             for i in localdata[sf]['count']:
                 sfHeaders.append(i[0])
-                del i[0]   
+                del i[0]
+            for i in localdata[aqf]['sums']:
+                print i[0]
+                adHeaders.append(i[0])
+                del i[0]      
             worksheet.write_row('A1', ['userid', 'Count'], bold)
             worksheet2.write_row('A1', sfHeaders, bold)
-            worksheet3.write_row('A1', ['Date', 'Sum'], bold)
+            worksheet3.write_row('A1', adHeaders, bold)
             worksheet4.write_row('A1', ['Response ID', 'Count'], bold)
 
             del localdata['Application Access Frequency']['count'][0]
-            del localdata[aqf]['sums'][0]
             del localdata[srf]['count'][0]
 
             #worksheet 1
@@ -120,8 +126,11 @@ def prepareData(request):
                     worksheet2.write(row+1, colnum+1, localdata[sf]['count'][colnum][row])
 
             #worksheet 3
+            print localdata[aqf]['dates']
             worksheet3.write_column('A2', localdata[aqf]['dates'])
-            worksheet3.write_column('B2', localdata[aqf]['sums'])
+            for colnum in range(0, len(localdata[aqf]['sums'])):
+                for row in range(0,len(localdata[aqf]['sums'][colnum])):
+                    worksheet3.write(row+1, colnum+1, localdata[aqf]['sums'][colnum][row])
             #shortsheet 4
             worksheet4.write_column('A2', localdata[srf]['responseIDs'])
             worksheet4.write_column('B2', localdata[srf]['count'])
@@ -129,9 +138,10 @@ def prepareData(request):
             #init each new graph
             chart1 = workbook.add_chart({'type': 'column'})
             chart2 = workbook.add_chart({'type': 'line'})
-            chart3 = workbook.add_chart({'type': 'line'})
+            
             chart4 = workbook.add_chart({'type': 'column'})
-
+            chart3 = workbook.add_chart({'type': 'line'})
+           
             #add series data
             for sym in range(0, len(localdata[sf]['count'])):
                 currCol = unichr(sym+66)
@@ -139,7 +149,14 @@ def prepareData(request):
                 'categories': '='+sf+'!$A$2:$A$'+str(len(localdata[sf]['dates'])+1), #x value
                 'values':     '='+sf+'!$'+currCol+'$2:$'+currCol+'$'+str(len(localdata[sf]['count'])+1), #y values
                 'gap':        500, #gap?
-                })    
+                }) 
+
+            for adhoc in range(0, len(localdata[aqf]['sums'])):
+                currCo = unichr(adhoc+66)
+                chart3.add_series({
+                'categories': '=Adhoc Query Frequency!$A$2:$A$72', #x value
+                'values':     '=Adhoc Query Frequency!$'+currCo+'$2:$'+currCo+'$'+str(len(localdata[aqf]['sums'])+1), #y values
+                })      
 
             chart1.add_series({
                 'categories': '='+aaf+'!$A$2:$A$'+str(len(localdata['Application Access Frequency']['userid'])+1), #x value
@@ -151,20 +168,17 @@ def prepareData(request):
                 'values':     '='+srf+'!$B$2:$B$'+str(len(localdata[srf]['count'])+1), #y values
                 'gap':        500, #gap?
                 })
-            chart3.add_series({
-                'categories': '='+aqf+'!$A$2:$A$'+str(len(localdata[aqf]['dates'])+1), #x value
-                'values':     '='+aqf+'!$B$2:$B$'+str(len(localdata[aqf]['sums'])+1), #y values
-                'gap':        500, #gap?
-                })
+
             chart4.set_title ({'name': srf})
             chart4.set_x_axis({'name': 'Response ID'})
             chart4.set_y_axis({'name': 'Count'})
             chart4.set_size({'x_scale': .5*len(localdata[srf]['responseIDs']), 'y_scale': 3})
-
+            print ('hi')
+            print len(localdata[aqf]['dates'])
             chart3.set_title ({'name': aqf})
             chart3.set_x_axis({'name': 'Dates'})
             chart3.set_y_axis({'name': 'Sums'})
-            chart3.set_size({'x_scale': .2*len(localdata[aqf]['dates']), 'y_scale': 3})
+            chart3.set_size({'x_scale': .1*len(localdata[aqf]['dates']), 'y_scale': 3})
 
             chart2.set_title ({'name': sf})
             chart2.set_x_axis({'name': 'Dates'})
@@ -181,11 +195,11 @@ def prepareData(request):
             chart3.set_style(11)
             chart2.set_style(11)
 
-            # Insert the chart into the worksheet (with an offset).
-            worksheet.insert_chart('D2', chart1, {'x_offset': 50, 'y_offset': 10})
-            worksheet4.insert_chart('D2', chart4, {'x_offset': 50, 'y_offset': 10})
-            worksheet3.insert_chart('D2', chart3, {'x_offset': 50, 'y_offset': 10})
-            worksheet2.insert_chart('Q2', chart2, {'x_offset': 50, 'y_offset': 10})
+            # # Insert the chart into the worksheet (with an offset).
+            # worksheet.insert_chart('D2', chart1, {'x_offset': 50, 'y_offset': 10})
+            # worksheet4.insert_chart('D2', chart4, {'x_offset': 50, 'y_offset': 10})
+            # worksheet2.insert_chart('Q2', chart2, {'x_offset': 50, 'y_offset': 10})
+            # worksheet3.insert_chart('Q2', chart3, {'x_offset': 50, 'y_offset': 10})
 
             workbook.close()
             make_archive('zipfile', 'gztar', 'media/rsvfiles')
